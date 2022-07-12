cf08f166ba89212990edee019f3ad4dd0e0b7f4a
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-07-12 21:49:44 +0400

    Added the tabbing functionality

--- index.js
+++ index.js
@@ -2,14 +2,24 @@
 
 // Add event listeners to each tab
 const tabs = Array.from(document.querySelectorAll('.tab'))
+const tabContainer = document.querySelector('.container')
+const tabContents = Array.from(tabContainer.querySelectorAll('.tab-content'))
+
 
 tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
         // Find the tab they clicked
-        console.log(tab)
-        if (tab.getAttribute('data-theme') = 'fitness') {
-            tab.style.backgroundColor = 'red'
-        }
+        const target = tab.dataset.theme
+
+        //Find the correct tab content
+        const tabContent = tabContainer.querySelector('#' + target)
+
+        //Select a tab and emphasizing the selected one
+        tabs.forEach(t => t.classList.remove('is-selected'))
+        tab.classList.add('is-selected')
+
+        tabContents.forEach(c => c.classList.remove('is-selected'))
+        tabContent.classList.add('is-selected')
     })
 
 })
